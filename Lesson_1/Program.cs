7de4f8e412731512b662be0abd5b0138bf21ba42
6N2P <umnik1985@mail.ru>
2022-05-29 00:15:48 +0500

    initial

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            Console.Write("Hello ");           
+            Console.ReadLine();
+            Console.Write("World ");
+            Console.ReadLine();
+            Console.Write("!!!");
+            Console.ReadKey();
+        }
+    }
+}
